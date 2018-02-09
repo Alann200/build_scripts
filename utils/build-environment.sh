f4d0bf26019ba60419bc9c1c0df8b3e5249d2a5b
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2018-02-09 19:19:53 +0100

    script: Add ADB & Fastboot installer

--- utils/build-environment.sh
+++ utils/build-environment.sh
@@ -93,5 +93,14 @@ fi
 git config --global user.name ${GIT_USER_NAME}
 git config --global user.email ${GIT_USER_MAIL}
 
+# Install and setup ADB & Fastboot
+sudo wget -O /etc/udev/rules.d/51-android.rules https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/51-android.rules
+sudo chmod a+r /etc/udev/rules.d/51-android.rules
+sudo service udev restart
+sudo apt-get install -y android-tools-adb android-tools-fastboot
+wget -O - https://skia.googlesource.com/skia/+archive/cd048d18e0b81338c1a04b9749a00444597df394/platform_tools/android/bin/linux.tar.gz | tar -zxvf - adb
+sudo mv adb /usr/bin/adb
+sudo chmod +x /usr/bin/adb
+
 # Cleaning up
 sudo apt-get clean && sudo apt-get autoclean
